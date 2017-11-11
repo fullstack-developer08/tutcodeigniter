6d2c9b96ded21fbab5ebf6db9672641598d6d3ef
mdarif-k <tech.aarifkhan@gmail.com>
2017-11-11 15:00:05 +0530

    issue fixed

--- application/views/tutorials/git/gitCopyFilesWithOutFolder.php
+++ application/views/tutorials/git/gitCopyFilesWithOutFolder.php
@@ -2,7 +2,7 @@
 	<div class="panel panel-heading">
         Git files with out repository name or folder.
 	</div>
-	<div class="panel panel-heading">
+	<div class="panel panel-content">
 		<pre class="prettyprint">
             > git init
             > git remote add origin GIT_URL
