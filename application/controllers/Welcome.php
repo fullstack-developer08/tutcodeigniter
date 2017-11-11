53173590451a9feba58f686c03c088532f9f82f2
mdarif-k <tech.aarifkhan@gmail.com>
2017-11-11 12:59:36 +0530

    fixed: conf setting

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('header');
+		$header['title']='';
+		$header['heading']='';
+		$header['keywords']='';
+		$header['description']='';
+		$this->load->view('header',$header);
 		$this->load->view('footer');
 	}
 }
