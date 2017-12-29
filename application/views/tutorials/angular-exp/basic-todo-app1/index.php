d065522814a4394708d5dcdab6ca000da16084b4
Arif Khan <tech.aarifkhan@gmail.com>
2017-12-29 22:40:34 +0530

    added angular-example folder

--- application/views/tutorials/angular-exp/basic-todo-app1/index.php
+++ application/views/tutorials/angular-exp/basic-todo-app1/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>TODO1</title>
-  <base href="/">
+  <base href="http://tutorials.way2programming.com/tutorials/angular-exp/basic-todo-app1/">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
